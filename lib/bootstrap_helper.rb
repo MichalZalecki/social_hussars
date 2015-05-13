52d680a8181c42643cc7521636af7673408cdeef
MichalZalecki <zalecki.post@gmail.com>
2015-05-13 15:56:20 +0200

    Bootstrap helper into separate module & autoload from /lib

---
+++ lib/bootstrap_helper.rb
@@ -0,0 +1,15 @@
+module BootstrapHelper
+  def bootstrap_alert_classes_for(flash_type)
+    classes_mapping = {
+      success: "alert-success",
+      error: "alert-danger",
+      alert: "alert-warning",
+      notice: "alert-info"
+    }
+    "alert " + classes_mapping[flash_type.to_sym] || flash_type.to_s
+  end
+  def bootstrap_navbar_link_to(*args)
+    class_attr = ( request.fullpath == args[1] ? ' class="active"' : '' )
+    "<li#{class_attr}>#{link_to(*args)}</li>".html_safe
+  end
+end
