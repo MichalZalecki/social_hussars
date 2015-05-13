7706c1310cb93c3801417f50f79ddb78438bcace
MichalZalecki <zalecki.post@gmail.com>
2015-05-13 20:34:14 +0200

    QuestonsController#index spec QuestionsController#show spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'page/home'
   resources :questions
   devise_for :users
   root 'questions#index'
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,46 +2,31 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
+  let(:question) { create(:question) }
+
+  describe 'GET #index' do
+    it 'returns http success and renders the :index template' do
       get :index
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+    end
+
+    it 'assigns all questions as @questions' do
+      get :index
+      expect(assigns(:questions)).to eq([question])
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
+  describe 'GET #show' do
+    it 'returns http success and renders the :show template' do
+      get :show, { id: question }
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+    end
+
+    it 'assigns the requested question as @question' do
+      get :show, { id: question }
+      expect(assigns(:question)).to eq(question)
     end
   end
-
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
