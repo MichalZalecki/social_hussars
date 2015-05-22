30f786f275bced1bc03aff17e1a7c96626f0779d
MichalZalecki <zalecki.post@gmail.com>
2015-05-22 19:31:50 +0200

    Avatar improvements

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,17 +60,4 @@ Rails.application.configure do
   config.generators.helper = false
   config.generators.view_specs = false
   config.generators.helper_specs = false
-
-  # S3
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_host_name => ENV['S3_HOST_NAME'],
-    :s3_credentials => {
-      :bucket             => ENV['S3_BUCKET_NAME'],
-      :access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
-    },
-    :url => ':s3_domain_url',
-    :path => "/:class/:attachment/:id_partition/:style/:filename"
-  }
 end
