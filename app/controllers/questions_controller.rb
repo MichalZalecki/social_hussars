6a78a2469254e1219890911dea466d4eab108ec3
MichalZalecki <zalecki.post@gmail.com>
2015-05-23 22:10:52 +0200

    authenticate index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:show]
   before_action :find_question, only: [:show, :edit, :update]
   before_action :authenticate_owner!, except: [:index, :show, :new, :create]
   before_action :check_points!, except: [:index, :show, :edit, :update]
