31dd476650f32bf969ff88b97ba8fed04fccf962
MichalZalecki <zalecki.post@gmail.com>
2015-05-23 14:12:30 +0200

    Redis Cloud connection

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,8 @@
-Resque.redis = 'localhost:6379'
+
+if ENV['REDISCLOUD_URL']
+  Resque.redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+else
+  Resque.redis = 'localhost:6379'
+end
+
 Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
