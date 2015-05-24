6cfc103edf5b58eea84f1487303dc311d4692b22
MichalZalecki <zalecki.post@gmail.com>
2015-05-24 17:02:11 +0200

    routes formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  # We need to define devise_for just omniauth_callbacks:auth_callbacks otherwise it does not work with scoped locales
+  # We need to define devise_for just omniauth_callbacks:auth_callbacks
+  # otherwise it does not work with scoped locales
   # see https://github.com/plataformatec/devise/issues/2813
-  devise_for :users, skip: [:session, :password, :registration, :confirmation], controllers: { omniauth_callbacks: 'callbacks' }
-
+  devise_for :users, skip: [:session, :password, :registration, :confirmation],
+             controllers: { omniauth_callbacks: 'callbacks' }
 
   scope "(:locale)", locale: /en|pl/ do
     get 'page/home'
@@ -16,11 +17,10 @@ Rails.application.routes.draw do
       end
     end
 
-    # We define here a route inside the locale thats just saves the current locale in the session
+    # We define here a route inside the locale
+    # thats just saves the current locale in the session
     get 'omniauth/:provider' => 'omniauth#localized', as: :localized_omniauth
-
     devise_for :users, skip: :omniauth_callbacks
-
     resources :users, only: [:show]
     get '/:locale' => 'questions#index'
     root 'questions#index'
