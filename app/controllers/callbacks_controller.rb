9c3faa80d2dfca98d997099133de451f95970892
MichalZalecki <zalecki.post@gmail.com>
2015-05-24 16:59:02 +0200

    l18n config with OmniAuth support

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,5 +1,8 @@
 class CallbacksController < Devise::OmniauthCallbacksController
     def github
+      # Use the session locale set earlier; use the default if it isn't available.
+      I18n.locale = session[:omniauth_login_locale] || I18n.default_locale
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Github"
       @user = User.from_omniauth(request.env["omniauth.auth"])
       sign_in_and_redirect @user
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,29 @@
 Rails.application.routes.draw do
-  get 'page/home'
-  get 'page/leaderboard'
-  resources :questions, except: :destroy do
-    resources :answers, only: [:create] do
-      post 'upvote'
-      post 'downvote'
-    post 'accept'
+
+  # We need to define devise_for just omniauth_callbacks:auth_callbacks otherwise it does not work with scoped locales
+  # see https://github.com/plataformatec/devise/issues/2813
+  devise_for :users, skip: [:session, :password, :registration, :confirmation], controllers: { omniauth_callbacks: 'callbacks' }
+
+
+  scope "(:locale)", locale: /en|pl/ do
+    get 'page/home'
+    get 'page/leaderboard'
+    resources :questions, except: :destroy do
+      resources :answers, only: [:create] do
+        post 'upvote'
+        post 'downvote'
+      post 'accept'
+      end
     end
+
+    # We define here a route inside the locale thats just saves the current locale in the session
+    get 'omniauth/:provider' => 'omniauth#localized', as: :localized_omniauth
+
+    devise_for :users, skip: :omniauth_callbacks
+
+    resources :users, only: [:show]
+    get '/:locale' => 'questions#index'
+    root 'questions#index'
+
   end
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
-  resources :users, only: [:show]
-  root 'questions#index'
 end
