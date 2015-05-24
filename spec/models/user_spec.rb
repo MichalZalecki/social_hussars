670d0f6c6ef202d74ce77d2606aaf2e4584d3aab
MichalZalecki <zalecki.post@gmail.com>
2015-05-24 13:09:13 +0200

    overdued User specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
   subject(:user){ build(:user) }
   let(:answer) { create(:answer) }
   let(:voter) { create(:user) }
+  let(:other_user) { create(:user) }
 
   it { should validate_presence_of(:username) }
   it { should have_attached_file(:avatar) }
@@ -30,19 +31,12 @@ RSpec.describe User, type: :model do
     end
   end
 
-  describe '#points_for_start' do
-
-    it 'sets default points' do
-      user.send(:points_for_start) # points_for_start is private
-      expect(user.points).to eq(100)
-    end
-  end
-
   describe '#points_for_upvote' do
 
     context 'voter did NOT downvote answer before' do
       it 'gives points for upvote' do
-        expect { user.points_for_upvote(voter, answer) }.to change(user, :points).by(5)
+        expect { user.points_for_upvote(voter, answer) }
+          .to change(user, :points).by(User::POINTS_FOR_VOTE)
       end
     end
 
@@ -51,7 +45,8 @@ RSpec.describe User, type: :model do
       before(:each) { answer.downvote_from voter }
 
       it 'gives points for upvote' do
-        expect { user.points_for_upvote(voter, answer) }.to change(user, :points).by(10)
+        expect { user.points_for_upvote(voter, answer) }
+          .to change(user, :points).by(User::POINTS_FOR_VOTE*2)
       end
     end
   end
@@ -60,7 +55,8 @@ RSpec.describe User, type: :model do
 
     context 'voter did NOT upvote answer before' do
       it 'takes points for upvote' do
-        expect { user.points_for_downvote(voter, answer) }.to change(user, :points).by(-5)
+        expect { user.points_for_downvote(voter, answer) }
+          .to change(user, :points).by(-User::POINTS_FOR_VOTE)
       end
     end
 
@@ -69,7 +65,8 @@ RSpec.describe User, type: :model do
       before(:each) { answer.liked_by voter }
 
       it 'takes points for upvote' do
-        expect { user.points_for_downvote(voter, answer) }.to change(user, :points).by(-10)
+        expect { user.points_for_downvote(voter, answer) }
+          .to change(user, :points).by(-User::POINTS_FOR_VOTE*2)
       end
     end
   end
@@ -77,7 +74,101 @@ RSpec.describe User, type: :model do
   describe '#points_for_accepted_answer' do
 
     it 'takes points for downvote' do
-      expect { user.points_for_accepted_answer }.to change(user, :points).by(25)
+      expect { user.points_for_accepted_answer }
+        .to change(user, :points).by(User::POINTS_FOR_ACCEPTED_ANSWER)
+    end
+  end
+
+  describe '#points_for_asking_question' do
+
+    it 'takes points for asking question' do
+      expect { user.points_for_asking_question }
+        .to change(user, :points).by(-User::POINTS_FOR_ASKING_QUESTION)
+    end
+  end
+
+  describe '#able_to_ask_question' do
+
+    context 'when user has enought points' do
+
+      before(:each) { user.points = User::POINTS_FOR_ASKING_QUESTION }
+
+      it 'returns true' do
+        expect(user.able_to_ask_question?).to be(true)
+      end
+    end
+
+    context 'when user hasn\'t enought points' do
+
+      before(:each) { user.points = User::POINTS_FOR_ASKING_QUESTION-1 }
+
+      it 'returns false' do
+        expect(user.able_to_ask_question?).to be(false)
+      end
+    end
+  end
+
+  describe '#superstar?' do
+
+    context 'when user has enought points to be a superstar' do
+
+      before(:each) { user.points = User::POINTS_TO_BE_A_SUPERSTAR }
+
+      it 'returns true' do
+        expect(user.superstar?).to be(true)
+      end
+    end
+
+    context 'when user hasn\'t enought points to be a superstar' do
+
+      before(:each) { user.points = User::POINTS_TO_BE_A_SUPERSTAR-1 }
+
+      it 'returns false' do
+        expect(user.superstar?).to be(false)
+      end
+    end
+  end
+
+  describe '#points_for_start' do
+
+    context 'when user has nil points' do
+
+      before(:each) { user.points = nil }
+
+      it 'sets default points' do
+        user.send(:points_for_start) # points_for_start is private
+        expect(user.points).to eq(User::POINTS_FOR_START)
+      end
+    end
+
+    context 'when user has NOT nil points' do
+
+      before(:each) { user.points = 30 }
+
+      it 'does not change points' do
+        user.send(:points_for_start) # points_for_start is private
+        expect(user.points).to eq(30)
+      end
+    end
+  end
+
+  describe '.unique_username' do
+
+    context 'when the username is unique' do
+
+      it 'returns the username' do
+        expect(User.unique_username(user.username)).to eq(user.username)
+      end
+    end
+
+    context 'when the username isn\'t unique' do
+
+      before(:each) { user.username = other_user.username }
+
+      it 'returns the username with unique token' do
+        expect(User.unique_username(user.username)).to match(user.username)
+        expect(User.unique_username(user.username)).not_to eq(user.username)
+      end
     end
   end
 end
