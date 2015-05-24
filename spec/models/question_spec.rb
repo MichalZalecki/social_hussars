0f00e987b629ee3f9b9c6f50b0d45f996f51dadb
MichalZalecki <zalecki.post@gmail.com>
2015-05-24 12:30:32 +0200

    overdued Question specs

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe Question do
 
+  let(:question) { create(:question) }
+  let(:question_with_answers) { create(:question, :with_answers) }
+  let(:other_user) { create(:user) }
+
   it 'has a valid factory' do
     expect(build(:question)).to be_valid
   end
@@ -14,5 +18,54 @@ describe Question do
     it { should belong_to(:user) }
     it { should have_many(:answers) }
 
+  end
+
+  describe '#owner?' do
+
+    context 'user is the question author' do
+
+      let(:user) { question.user }
+
+      it 'returns true' do
+        expect(question.owner?(user)).to be(true)
+      end
+    end
+
+    context 'user is NOT the question author' do
+
+      let(:user) { other_user }
+
+      it 'returns false' do
+        expect(question.owner?(user)).to be(false)
+      end
+    end
+  end
+
+  describe '#accepted?' do
+
+    context 'question has at least one accepted answers' do
+
+      before(:each) { question_with_answers
+                        .answers.first.update(accepted: true) }
+
+      it 'returns true' do
+        expect(question_with_answers.accepted?).to be(true)
+      end
+    end
+
+    context 'question hasn\'t any accepted answers' do
+
+      it 'returns false' do
+        expect(question_with_answers.accepted?).to be(false)
+      end
+    end
+  end
+
+  describe '#points_for_asking_question' do
+
+    it 'calls :points_for_asking_question on author' do
+      expect(question.user).to receive(:points_for_asking_question)
+      question.send(:points_for_asking_question)
+    end
   end
 end
