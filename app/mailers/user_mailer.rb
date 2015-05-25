1598ab603b9a9b355f2cb76d662662075fd85d66
MichalZalecki <zalecki.post@gmail.com>
2015-05-25 15:44:06 +0200

    mails templates

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,5 @@
 class UserMailer < ApplicationMailer
+  default from: 'no-reply@social-hussars.herokuapp.com'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -6,8 +7,7 @@ class UserMailer < ApplicationMailer
   #   en.user_mailer.question_answered.subject
   #
   def question_answered(answer)
-    @greeting = "Hi"
-
+    @answer = answer
     mail to: answer.question.user.email
   end
 
@@ -17,8 +17,7 @@ class UserMailer < ApplicationMailer
   #   en.user_mailer.answer_accepted.subject
   #
   def answer_accepted(answer)
-    @greeting = "Hi"
-
+    @answer = answer
     mail to: answer.user.email
   end
 end
--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -2,12 +2,12 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/question_answered
   def question_answered
-    UserMailer.question_answered
+    UserMailer.question_answered Answer.last
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/answer_accepted
   def answer_accepted
-    UserMailer.answer_accepted
+    UserMailer.answer_accepted Answer.last
   end
 
 end
