319f6c0abe319f359df765a59cfe72d710c3354b
MichalZalecki <zalecki.post@gmail.com>
2015-05-25 15:58:10 +0200

    small fixes

--- spec/controllers/page_controller_spec.rb
+++ spec/controllers/page_controller_spec.rb
@@ -2,14 +2,6 @@ require 'rails_helper'
 
 describe PageController do
 
-  describe 'GET #home' do
-
-    before(:each) { get :home }
-
-    it { should respond_with(:success) }
-    it { should render_template(:home) }
-  end
-
   describe 'GET #leaderboard' do
 
     before(:each) { get :leaderboard }
